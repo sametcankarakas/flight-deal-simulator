f6b2325514c1192a83b75c85e60511ce728ec011
Samet Can KARAKAŞ <scankarakas@hotmail.com>
2021-11-25 21:21:33 +0300

    data_manager.py file now ready to get result from sheet and on main page we can print input city with price .

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,17 @@
+import requests
+import os
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        # self.sheety_auth = os.environ["SHEETY_BEARER_AUTH"]
+        self.sheety_endpoint = "https://api.sheety.co/59490a34d8d35b5278e99d96c81bd616/flightDeals/prices"
+
+
+    def sheety_response(self):
+        self.sheety_response = requests.get(url=self.sheety_endpoint)
+        self.sheety_response.raise_for_status()
+        self.response = self.sheety_response.json()
+        return self.response
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,46 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+import requests
+import os
+from data_manager import DataManager
+import datetime
+
+#
+# tequila_api_key = os.environ["TEQUILA_API_KEY"]
+# tequila_search_endpoint = "https://tequila-api.kiwi.com/v2/search"
+#
+#
+# header = {
+#     "apikey": tequila_api_key
+# }
+# flight_parameters = {
+#     "fly_from": "TR",
+#     "fly_to": "FRA",
+#     "date_from": "05/02/2022",
+#     "date_to": "15/02/2022",
+#     "limit": 10
+# }
+#
+# response_flight = requests.get(url=tequila_search_endpoint, headers=header, params=parameters)
+# response_flight.raise_for_status()
+#
+# flights_with_price = response_flight.json()["data"]
+# print(response_flight.text)
+# flight_list = []
+#
+# for number in flights_with_price:
+#     if number["price"] < 60:
+#         flight_list.append(number)
+#
+# print(flight_list)
+
+where_to = input("Enter city name where you want to fly?: ").title()
+
+#
+# sheety_result = DataManager()
+# result = sheety_result.sheety_response()
+result = {'prices': [{'city': 'Paris', 'iataCode': 'PAR', 'lowestPrice': 66, 'id': 2}, {'city': 'Berlin', 'iataCode': 'BER', 'lowestPrice': 88, 'id': 3}, {'city': 'Tokyo', 'iataCode': 'TYO', 'lowestPrice': 485, 'id': 4}, {'city': 'Sydney', 'iataCode': 'SYD', 'lowestPrice': 551, 'id': 5}, {'city': 'London', 'iataCode': 'LON', 'lowestPrice': 333, 'id': 6}, {'city': 'Kuala Lumpur', 'iataCode': 'KUL', 'lowestPrice': 414, 'id': 7}, {'city': 'New York', 'iataCode': 'NYC', 'lowestPrice': 343, 'id': 8}, {'city': 'San Francisco', 'iataCode': 'SFO', 'lowestPrice': 453, 'id': 9}, {'city': 'Cape Town', 'iataCode': 'CPT', 'lowestPrice': 411, 'id': 10}]}
+for _ in result["prices"]:
+    if _["city"] == where_to.title():
+        print(f"the price of {where_to} in the sheet is: €"f"{_['lowestPrice']}")
+        break
+    print(_)
