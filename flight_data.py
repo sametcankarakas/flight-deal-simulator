d286823daf73c2f0ae6a83ce0f9e5f6cf8dcc0c1
Samet Can KARAKAŞ <scankarakas@hotmail.com>
2021-11-26 17:20:24 +0300

    its ready to search and find cheapest flight in 6 months . sms not included.

--- flight_data.py
+++ flight_data.py
@@ -1,15 +1,16 @@
 import os
 class FlightData:
     #This class is responsible for structuring the flight data.
-    def __init__(self, city_iata):
-        self.tequila_api_key = os.environ["TEQUILA_API_KEY"]
+    def __init__(self, city_iata, date_from, date_to):
+        self.flight_env_api_key = os.environ["TEQUILA_API_KEY"]
+        self.tequila_header = {"apikey": self.flight_env_api_key}
         self.tequila_search_endpoint = "https://tequila-api.kiwi.com/v2/search"
         self.city_from = "Istanbul"
         self.fly_from = "SAW"
         # self.city_to = city
         self.city_to_iata = city_iata
-        self.date_from = "27/11/2021"
-        self.date_to = "27/04/2022"
+        self.date_from = date_from
+        self.date_to = date_to
 
     def flight_parameters(self):
         self.parameters = {
@@ -17,5 +18,7 @@ class FlightData:
             "fly_to": self.city_to_iata,
             "date_from": self.date_from,
             "date_to": self.date_to,
+            "curr": "EUR",
+            "limit": 5
         }
         return self.parameters
--- flight_search.py
+++ flight_search.py
@@ -1,8 +1,18 @@
 import requests
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    def check_flights(self, url, headers, flight_parameters):
-        self.response = requests.get(url, headers, flight_parameters)
+    def __init__(self, url, headers, flight_parameters):
+        self.url = url
+        self.headers = headers
+        self.flight_parameters = flight_parameters
+        self.response = requests.get(url=self.url, headers=self.headers, params=self.flight_parameters)
+        self.response.raise_for_status()
+        self.flight_result = self.response.json()
+        print(self.flight_result)
 
 
+    # def request_check(self):
 
+
+    # def price_check(self):
+    #     self.price = self.price_check()
